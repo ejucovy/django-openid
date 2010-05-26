363bf9cfcf219495186ccd4aebb316e415359bae
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2010-05-26 23:04:23 +0000

    add method signatures

--- django_openid/demos/provider/anon_provider/__init__.py
+++ django_openid/demos/provider/anon_provider/__init__.py
@@ -2,13 +2,13 @@ from django_openid.provider import Provider
 from django.shortcuts import render_to_response as render
 
 class AnonProvider(Provider):
-    def user_is_logged_in(self, *args):
+    def user_is_logged_in(self, request):
         return True
     
-    def user_owns_openid(self, *args):
+    def user_owns_openid(self, request, openid):
         return True
     
-    def user_trusts_root(self, *args):
+    def user_trusts_root(self, request, openid, trust_root):
         return True
 
     def get_sreg_data(self, request, openid):
