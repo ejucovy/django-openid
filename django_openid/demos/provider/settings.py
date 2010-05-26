ab634969ea88216186bbaeee259bd3b1154af309
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2010-05-26 23:36:17 +0000

    add contrib.auth and contrib.admin and registration/ templates

--- django_openid/demos/provider/settings.py
+++ django_openid/demos/provider/settings.py
@@ -63,14 +63,19 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'urls'
 
+import os
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.getcwd(), 'templates'),
 )
 
 INSTALLED_APPS = (
     'django.contrib.sessions',
+    'django.contrib.contenttypes',
+    'django.contrib.auth',
+    'django.contrib.admin',
     'django_openid',
     'anon_provider',
 )
