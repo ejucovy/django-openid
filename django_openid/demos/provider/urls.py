5b94b0a96ebaf002d687474572dc48a05c2355ca
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2010-05-26 23:37:01 +0000

    hook in admin/ and accounts/ urls; name the openid view

--- django_openid/demos/provider/urls.py
+++ django_openid/demos/provider/urls.py
@@ -2,8 +2,14 @@ from django.conf.urls.defaults import *
 from django.http import HttpResponseRedirect
 from anon_provider import AnonProvider, openid_page
 
+from django.contrib import admin
+admin.autodiscover()
+
 urlpatterns = patterns('',
-    (r'^$', lambda r: HttpResponseRedirect('/openid/')),
-    (r'^server/$', AnonProvider()),
-    (r'^(\w+)/$', openid_page),
+                       (r'^$', lambda r: HttpResponseRedirect('/openid/')),
+                       (r'^server/$', AnonProvider()),
+                       (r'^accounts/', include('django.contrib.auth.urls')),
+                       (r'^admin/', include(admin.site.urls)),
+                       url(r'^(\w+)/$', openid_page,
+                           name='openid_page'),
 )
