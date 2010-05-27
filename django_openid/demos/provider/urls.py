3057b27b1fdfb468811e71a2bfce4c62a192a877
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2010-05-27 15:08:06 -0400

    use authprovider

--- django_openid/demos/provider/urls.py
+++ django_openid/demos/provider/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls.defaults import *
 from django.http import HttpResponseRedirect
-from anon_provider import AnonProvider, openid_page
+from auth_user_provider import AuthProvider, openid_page
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
                        (r'^$', lambda r: HttpResponseRedirect('/openid/')),
-                       (r'^server/$', AnonProvider()),
+                       (r'^server/$', AuthProvider()),
                        (r'^accounts/', include('django.contrib.auth.urls')),
                        (r'^admin/', include(admin.site.urls)),
                        url(r'^(\w+)/$', openid_page,
